29d045d481a051108474349d1fb8b200aab890d1
NordicAngel <78414012+NordicAngel@users.noreply.github.com>
2022-11-02 12:43:09 +0100

    First comit

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const app = Vue.createApp({
     data() {
         return {
+            number1:"",
+            number2:"",
+            result:0  
             }
-
-        }
+        },
+        methods: {}
     },
 );
